bfc3139d3fb8ff4446ba52e97fe25643ab6e9783
Douglas Uretsky <douglasuretsky@gmail.com>
2019-06-04 12:39:22 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+
+function depthFirstSearch(rootNode, vertices, edges){
+    let stack = []
+    stack.push(rootNode)
+    let visited = [rootNode]
+
+    while(stack.length != 0){
+        let vertice = stack.pop()
+        if(!vertice.discovered){
+            vertice.discovered = true
+
+            findAdjacent(vertice.name, vertices, edges).forEach(function(node){
+                visited.push(node)
+                stack.push(node)
+            })
+        }
+    }
+    return visited;
+}
+
+function findAdjacent(nodeName,  vertices, edges){
+    return edges.filter(function(edge){
+      return edge.includes(nodeName)
+    }).map(function(edge){
+      return edge.filter(function(node){
+        return (node != nodeName)
+      })[0]
+    }).map(function(name){
+      return findNode(name, vertices)
+    }).filter(function(node){
+          return !node.discovered
+      })
+  }
+  
+  function findNode(nodeName, vertices){
+    return vertices.find(function(vertex){
+      return vertex.name == nodeName
+    })
+  }
